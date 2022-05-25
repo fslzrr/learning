ad64b47aae2f27d4792445bbf66826ae0d9baf27
Fernando Salazar <fslzrr@gmail.com>
2022-05-25 15:09:17 -0500

    rust: chapter 1

---
+++ rust/the-rust-programming-language/c1-getting-started/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("use 'rustc <file name>' to build rust file.");
+    println!("use 'cargo new <project name>' to create new rust project with cargo.");
+    println!("use 'cargo check' to to validate the rust project with cargo won't crash at build.");
+    println!("use 'cargo run' to build and run rust project with cargo.");
+    println!("use 'cargo build' to build project with cargo.");
+    println!("use 'cargo build --release' to build the production executable project with cargo.");
+}
